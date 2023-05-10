494393399047bed6f7207b39f38e0e22136178c7
Artur Paschoal <128155308+paschoal-artur@users.noreply.github.com>
2023-05-10 10:15:39 -0300

    code final

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Main functions"""
 def is_palindrome(string: str) -> bool:
     string = string.replace(" ", "").lower()
-    string = string.replace("!", "")
+    string = string.replace("!", "").lower()
     string = string.replace(".", "").lower()
     if(string==string[::-1]):
         return True
